8b0f19edd6a587fe6adf71848240a79292c7d56c
Muhammad yasir <Yasirkhany11111@gmail.com>
2025-03-11 15:40:19 +0500

    update table

--- src/dataTable.js
+++ src/dataTable.js
@@ -1,150 +1,138 @@
-'use strict';
-
-////////////////////////////////////////////
-// Selector
-const dataTable = document.querySelector('.data-table');
-const filter = document.querySelector('.header__wrap-filter');
-const searchInput = document.querySelector('.input__search');
-const btnAddCustomer = document.querySelector('.btn-add');
-const headerCheckbox = document.querySelector('.data-header__checkbox-input');
-const btnSort = document.querySelector('.fa-sort');
-
-const actionBox = document.querySelector('.action-box');
-const btnEdit = document.querySelector('.btn-edit');
-const btndelete = document.querySelector('.btn-delete');
-
-// Form
-const form = document.querySelector('.form');
-const id = document.querySelector('.form__input-1');
-const uname = document.querySelector('.form__input-2');
-const location = document.querySelector('.form__input-3');
-const age = document.querySelector('.form__input-4');
-const description = document.querySelector('.form__input-5');
-
-// Pagination
-const footer = document.querySelector('.footer');
-const paginationRightPerPage = document.querySelector(
-  '.pagination-right__pages'
-);
-const perPageList = document.querySelector('.per-page-list');
-
 ////////////////////////////////////////////
 // Class
 
-class DataTable {
-  #data = [];
-  #totalData = [];
-  #currentPage = 1;
-  #totalItems = 0;
-  #isFiltering = false;
-  #filteredQuery = '';
-  #sortOrder = null;
+export class DataTable {
   #options = null;
+  #data = [];
+  // #totalData = [];
+  // #currentPage = 1;
+  // #totalItems = 0;
+  // #isFiltering = false;
+  // #filteredQuery = '';
+  // #sortOrder = null;
 
   constructor(selector, options) {
-    this.tableElement = document.querySelector(selector);
+    this.table = document.querySelector(selector);
     this.#options = options;
 
-    this.#init();
+    this.fetchData();
   }
 
-  #init() {
-    // AdEventListener
-    window.addEventListener('DOMContentLoaded', this.#fetchData.bind(this));
-    filter.addEventListener('change', this.#filterData.bind(this));
-    btnAddCustomer.addEventListener('click', this.#addCustomer.bind(this));
-    searchInput.addEventListener('input', this.#searchData.bind(this));
-    headerCheckbox.addEventListener('change', this.#action.bind(this));
-    btnSort.addEventListener('click', this.#sortData.bind(this));
-    dataTable.addEventListener('click', this.#action.bind(this));
-    footer.addEventListener('click', this.#pagination.bind(this));
-    paginationRightPerPage.addEventListener('click', this.#dropDown.bind(this));
-  }
-
-  // Fetch full data
-  async fetchFullData() {
+  // Fetch Data
+  async fetchData() {
     try {
-      const response = await fetch(`${this.#options.api}items`);
-
-      if (!response.ok) throw new Error('Error fetching full data.');
-
-      this.#totalData = await response.json();
-    } catch (error) {
-      this.#showError(error.message);
-    }
-  }
-
-  // Fetch paginated data
-  async #fetchData() {
-    try {
-      const perPage = this.#options.pagination.perPage;
-
-      let apiURL = this.#isFiltering
-        ? `${this.#options.api}items?age_gte=${this.#filteredQuery}&_page=${
-            this.#currentPage
-          }&_limit=${perPage}`
-        : `${this.#options.api}items?_page=${
-            this.#currentPage
-          }&_limit=${perPage}`;
-
-      if (this.#sortOrder) {
-        apiURL += `&_sort=id&_order=${this.#sortOrder}`;
-      }
-
-      const response = await fetch(apiURL);
+      let response = await fetch(`${this.#options.api}items`);
 
       if (!response.ok)
         throw new Error('Error fetching data. Please try again!');
 
-      this.#totalItems = +response.headers.get('X-total-count') || 0;
+      const data = await response.json();
+      this.#data = data;
 
-      this.#data = await response.json();
-
-      this.renderData();
-      this.#updatePagination();
-      this.fetchFullData();
+      this.renderData(data);
     } catch (error) {
-      this.#showError(error.message);
+      this.showError(error.message);
     }
   }
 
   // Render Data
-  renderData() {
-    const tableBody = this.tableElement;
+  renderData(data) {
+    const tableBody = this.table;
     tableBody.innerHTML = '';
 
-    if (!this.#data.length) return;
+    if (!data.length) return;
 
-    this.#data.forEach((row, i) => {
-      const html = `
-        <div class="data-row data-row-${i + 1}" data-index="${i + 1}">
-          <div class="data-row__checkbox">
-            <input type="checkbox" class="data-row__checkbox-input" />
-            <div class="data-row__checkbox">
-              <span>${row.id}</span>
-            </div>
-          </div>
+    const html = `
+         <thead class="table-head">
+          <tr class="table-head__row">
+            <th class="table-head__column table-head__column-1">
+              <input type="checkbox" class="table-head__checkbox" />
+              <i class="fa-solid fa-sort"></i>
+            </th>
+            ${this.#options.columns
+              .slice(1, -1)
+              .map(
+                (col, i) => `    
+              <th class="table-head__column table-head__column-${i + 2}">
+              <span>${col.heading}</span>
+              <i class="fa-solid fa-sort"></i>
+            </th>`
+              )
+              .join('')}
+          </tr>
+        </thead>
 
-          <p class="data-row__cell data-row__cell-1">
-            ${row.name}
-          </p>
-          <p class="data-row__cell data-row__cell-2">${row.location}</p>
-          <p class="data-row__cell data-row__cell-3">${row.age}</p>
-          <p class="data-row__cell data-row__cell-4">
-           ${row.description}
-          </p>
-        </div>`;
-      tableBody.insertAdjacentHTML('beforeend', html);
-    });
+        <tbody class="table-body">
+         ${data
+           .map(
+             (row, rowIndex) => `
+          <tr class="table-body__row" data-id="${row.id}">
+            <td class="table-body__column table-body__column-1">
+              <input type="checkbox" class="table-body__checkbox" />
+              <span>${rowIndex + 1}</span>
+            </td>
+            ${this.#options.columns
+              .slice(1)
+              .map(
+                (col, colIndex) =>
+                  `<td class="table-body__column table-body__column-${
+                    colIndex + 2
+                  }">${
+                    col.customCell
+                      ? col.customCell(row)
+                      : row[col.accessor] || ''
+                  }</td>`
+              )
+              .join('')}
+          </tr>`
+           )
+           .join('')}
+        </tbody>
+
+        <tfoot class="table-foot">
+          <tr class="table-foot__row">
+            <td class="table-foot__left-pages">1 - 10 of 97</td>
+
+            <td class="table-foot__right">
+              <button class="btn-dropdown">
+                Rows per page: <span>10</span>
+                <i class="fa-solid fa-chevron-down"></i>
+              </button>
+
+              <!-- Action -->
+              <div class="table-foot__right-list">
+                <div class="table-foot__right-inner-list">
+                  <button>9</button>
+                  <button>15</button>
+                  <button>20</button>
+                  <button>25</button>
+                </div>
+              </div>
+
+              <div class="table-foot__right-pages">
+                <button class="btn-left" data-index="1">
+                  <i class="fa-solid fa-chevron-left"></i>
+                </button>
+                <span>1 - 9</span>
+                <button class="btn-right" data-index="1">
+                  <i class="fa-solid fa-chevron-right"></i>
+                </button>
+              </div>
+            </td>
+          </tr>
+        </tfoot>`;
+
+    tableBody.insertAdjacentHTML('beforeend', html);
   }
 
+  /*
   // Search Data
-  async #searchData() {
+  async searchData() {
     try {
       const keyWord = searchInput.value.toLowerCase();
 
       if (keyWord === '') {
-        this.#fetchData();
+        this.fetchData();
         return;
       }
 
@@ -159,9 +147,11 @@ class DataTable {
       this.#showError(error.message);
     }
   }
+ */
 
+  /*
   // Add Data
-  async #addCustomer(e) {
+  async addCustomer(e) {
     try {
       form.style.display = 'block';
 
@@ -191,7 +181,7 @@ class DataTable {
           if (!response.ok) throw new Error('Error Adding data');
 
           await response.json();
-          this.#fetchData();
+          this.fetchData();
         }
 
         if (e.target.classList.contains('form__close-button')) {
@@ -202,12 +192,14 @@ class DataTable {
       // Add event listener
       form.addEventListener('click', this.formClickHandler);
     } catch (error) {
-      this.#showError(error.message);
+      this.showError(error.message);
     }
   }
+ */
 
-  // Pagination Data
-  async #pagination(e) {
+  /*
+   // Pagination Data
+  async pagination(e) {
     try {
       const target =
         e.target.closest('.btn-right') || e.target.closest('.btn-left');
@@ -229,15 +221,17 @@ class DataTable {
         this.#currentPage--;
       }
 
-      this.#fetchData();
+      this.fetchData();
       this.#updatePagination();
     } catch (error) {
-      this.#showError(error.message);
+      this.showError(error.message);
     }
   }
+  */
 
+  /*
   // UpdatePagination
-  #updatePagination() {
+  updatePagination() {
     const perPage = this.#options.pagination.perPage;
     const start = (this.#currentPage - 1) * perPage + 1;
     let end = this.#currentPage * perPage;
@@ -252,9 +246,11 @@ class DataTable {
       '.pagination-right__pagination span'
     ).textContent = `${start} - ${end}`;
   }
+  */
 
+  /*
   // DropDown
-  #dropDown(e) {
+  dropDown(e) {
     const target = e.target.closest('.pagination-right__pages');
     if (target.classList.contains('pagination-right__pages')) {
       perPageList.classList.toggle('active');
@@ -265,24 +261,28 @@ class DataTable {
 
       this.#options.pagination.perPage = pageNum;
 
-      this.#fetchData();
+      this.fetchData();
       perPageList.classList.remove('active');
     });
   }
+  */
 
-  // Sort Data
-  async #sortData(e) {
+  /*
+   // Sort Data
+  async sortData(e) {
     try {
       if (!e.target.classList.contains('fa-sort')) return;
 
       this.#sortOrder = this.#sortOrder === 'desc' ? 'asc' : 'desc';
 
-      this.#fetchData();
+      this.fetchData();
     } catch (error) {
-      this.#showError(error.message);
+      this.showError(error.message);
     }
   }
+ */
 
+  /*
   // Filter Data
   async #filterData(e) {
     try {
@@ -292,18 +292,18 @@ class DataTable {
       this.#filteredQuery = targetNum;
       this.#currentPage = 1;
 
-      await this.#fetchData();
+      await this.fetchData();
     } catch (error) {
       this.#showError(error.message);
     }
   }
+ */
 
+  /*
   // Edit Data
-  async #editData(target) {
+  async editRow(target) {
     try {
       form.style.display = 'block';
-      form.querySelector('.form__button').textContent = 'Edit';
-      uname.focus();
 
       // Get row data
       const dataRow = target;
@@ -323,52 +323,33 @@ class DataTable {
         .querySelector('.data-row__cell-4')
         .textContent.trim();
 
-      // Add old values to form
-      uname.value = oldName;
-      location.value = oldLocation;
-      age.value = oldAge;
-      description.value = oldDescription;
-
-      // Remove previous event listener
-      form.removeEventListener('submit', this._submitHandler);
-
-      this._submitHandler = async e => {
-        e.preventDefault();
-        form.style.display = 'none';
-        form.querySelector('.form__button').textContent = 'Add';
-        actionBox.style.display = 'none';
-        target.querySelector('.data-row__checkbox-input').checked = false;
-
-        // Get updated values
-        const updatedData = {
-          id: id.value.trim(),
-          name: uname.value.trim(),
-          location: location.value.trim(),
-          age: age.value.trim(),
-          description: description.value.trim(),
-        };
-
-        const response = await fetch(`${this.#options.api}items/${dataIndex}`, {
-          method: 'PUT',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(updatedData),
-        });
-
-        if (!response.ok) throw new Error('Error updating item');
-        const data = await response.json();
-        this.#fetchData();
-        uname.value = location.value = age.value = description.value = '';
+      // Get updated values
+      const updatedData = {
+        id: id.value.trim(),
+        name: uname.value.trim(),
+        location: location.value.trim(),
+        age: age.value.trim(),
+        description: description.value.trim(),
       };
 
-      // Add new event listener
-      form.addEventListener('submit', this._submitHandler);
+      const response = await fetch(`${this.#options.api}items/${dataIndex}`, {
+        method: 'PUT',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updatedData),
+      });
+
+      if (!response.ok) throw new Error('Error updating item');
+      const data = await response.json();
+      // this.fetchData();
     } catch (error) {
-      this.#showError(error.message);
+      this.showError(error.message);
     }
   }
+*/
 
-  // Delete Data
-  async #deleteData(target = false) {
+  /*
+  // // Delete Row
+  async deleteRow(target = false) {
     try {
       if (target) {
         const dataRow = target;
@@ -381,99 +362,27 @@ class DataTable {
         headerCheckbox.checked = false;
         actionBox.style.display = 'none';
 
-        for (const item of this.#totalData) {
+        for (const item of this.#data) {
           await fetch(`${this.#options.api}items/${item.id}`, {
             method: 'DELETE',
           });
         }
       }
 
-      this.#fetchData();
+      this.fetchData();
     } catch (error) {
-      this.#showError(error.message);
+      this.showError(error.message);
     }
   }
-
-  // Action
-  #action(e) {
-    if (e.target.classList.contains('data-row__checkbox-input')) {
-      const target = e.target.closest('.data-row');
-
-      const checkInput = target.querySelector('.data-row__checkbox-input');
-
-      if (!checkInput.checked) {
-        actionBox.style.display = '';
-      } else {
-        actionBox.style.display = 'block';
-
-        // Remove previous event listener
-        btnEdit.removeEventListener('click', this._editHandler);
-        btndelete.removeEventListener('click', this._deleteHandler);
-
-        this._editHandler = () => this.#editData(target);
-        this._deleteHandler = () => this.#deleteData(target);
-
-        // Add new event listener
-        btnEdit.addEventListener('click', this._editHandler);
-        btndelete.addEventListener('click', this._deleteHandler);
-      }
-    } else {
-      if (e.target.classList.contains('data-header__checkbox-input')) {
-        const checkInput = headerCheckbox;
-        const allCheckbox = document.querySelectorAll(
-          '.data-row__checkbox-input'
-        );
-
-        if (!checkInput.checked) {
-          actionBox.style.display = '';
-          allCheckbox.forEach(checkbox => {
-            checkbox.checked = false;
-          });
-        } else {
-          actionBox.style.display = 'block';
-          allCheckbox.forEach(checkbox => {
-            checkbox.checked = true;
-          });
-
-          btndelete.addEventListener(
-            'click',
-            function (e) {
-              this.#deleteData();
-            }.bind(this)
-          );
-        }
-      }
-    }
-  }
+ */
 
   // Error message
-  #showError(error) {
+  showError(error) {
     const elmnt = document.createElement('p');
     elmnt.classList.add('message');
     elmnt.textContent = error;
 
-    this.tableElement.innerHTML = '';
-    this.tableElement.append(elmnt);
+    this.table.innerHTML = '';
+    this.table.append(elmnt);
   }
 }
-
-const options = {
-  api: 'http://localhost:3000/',
-  pagination: { perPage: 9 },
-  columns: [
-    { heading: 'ID', accessor: 'id' },
-    { heading: 'Name', accessor: 'name' },
-    { heading: 'Age', accessor: 'age' },
-    { heading: 'Location', accessor: 'location' },
-    {
-      heading: 'Actions',
-      customCell: row => `
-          <button onclick="editRow(${row.id})">Edit</button>
-          <button onclick="deleteRow(${row.id})">Delete</button>
-        `,
-    },
-  ],
-};
-
-const table = new DataTable('.data-table', options);
-console.log(table);
---
+++ src/index.js
@@ -0,0 +1,71 @@
+'use strict';
+
+////////////////////////////////////////////
+// Import
+import { DataTable } from './dataTable.js';
+
+////////////////////////////////////////////
+// Selector
+/*
+const dataTable = document.querySelector('.data-table');
+const filter = document.querySelector('.header__wrap-filter');
+const searchInput = document.querySelector('.input__search');
+const btnAddCustomer = document.querySelector('.btn-add');
+const headerCheckbox = document.querySelector('.data-header__checkbox-input');
+const btnSort = document.querySelector('.fa-sort');
+
+const actionBox = document.querySelector('.action-box');
+const btnEdit = document.querySelector('.btn-edit');
+const btndelete = document.querySelector('.btn-delete');
+
+// Form
+const form = document.querySelector('.form');
+const id = document.querySelector('.form__input-1');
+const uname = document.querySelector('.form__input-2');
+const location = document.querySelector('.form__input-3');
+const age = document.querySelector('.form__input-4');
+const description = document.querySelector('.form__input-5');
+
+// Pagination
+const footer = document.querySelector('.footer');
+const paginationRightPerPage = document.querySelector(
+  '.pagination-right__pages'
+);
+const perPageList = document.querySelector('.per-page-list');
+*/
+
+////////////////////////////////////////////
+const options = {
+  api: 'http://localhost:3000/',
+  pagination: { perPage: 9 },
+  columns: [
+    { heading: 'ID', accessor: 'id' },
+    { heading: 'Name', accessor: 'name' },
+    { heading: 'Location', accessor: 'location' },
+    { heading: 'Age', accessor: 'age' },
+    { heading: 'Description', accessor: 'description' },
+    {
+      heading: 'Actions',
+      customCell: row => `
+          <button onclick="editRow(${row.id})">Edit</button>
+          <button onclick="deleteRow(${row.id})">Delete</button>
+        `,
+    },
+  ],
+};
+
+const table = new DataTable('.table', options);
+console.log(table);
+
+// AdEventListener
+/*
+window.addEventListener('DOMContentLoaded', table.fetchData.bind(this));
+filter.addEventListener('change', this.filterData.bind(this));
+btnAddCustomer.addEventListener('click', this.addCustomer.bind(this));
+searchInput.addEventListener('input', this.searchData.bind(this));
+headerCheckbox.addEventListener('change', this.action.bind(this));
+btnSort.addEventListener('click', this.sortData.bind(this));
+dataTable.addEventListener('click', this.action.bind(this));
+footer.addEventListener('click', this.pagination.bind(this));
+paginationRightPerPage.addEventListener('click', this.dropDown.bind(this));
+*/
